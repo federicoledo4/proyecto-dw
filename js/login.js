bfbacb980b9ba31a21557950ba8236ba02ffcd85
federicoledo4 <88751932+federicoledo4@users.noreply.github.com>
2021-09-02 04:05:14 -0300

    entrega_2

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@
 document.addEventListener("DOMContentLoaded", function(e){
   
 });
-//funcion que verifica los datos introducidos con un aviso y memoria
+//función que verifica los datos introducidos con un aviso y memoria
 let contador = 1
 function verificar() {
 
@@ -29,7 +29,7 @@ function verificar() {
     }
 }
 
-//Detalles esteticos
+//Detalles estéticos del botón de desconectar
 function sadMoment(booleano){
     if (booleano) {
       document.getElementById("desconectarse").style = "color:  rgba(255, 235, 205, 0); background-image:url('img/desconectar.gif'); border-color: rgb(255, 0, 0); background-size: contain;";
@@ -39,30 +39,48 @@ function sadMoment(booleano){
 }
  //Fin de los detalles estéticos
  
- //declaración y modificacion de variables 
+ //declaración, modificación de variables, pone el nombre de usuario y saluda
 let cuenta = false;
 let usuario = {};
 
 if (sessionStorage.usuario !== undefined) {
   usuario = JSON.parse(sessionStorage.getItem("usuario"));
   cuenta = true;
+  //Pone el nombre de usuario sessionStorage
+  document.getElementById("usuario").innerHTML = usuario.nombre;
+  //Saludo sessionStorage
   if (cuenta && usuario.primeraVez) {
-    alert("Bienvenido/a " + usuario.nombre);
-    usuario.primeraVez = false
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Bienvenido ' + usuario.nombre,
+      showConfirmButton: false,
+      timer: 3000
+    })
+    usuario.primeraVez = false;
     sessionStorage.setItem("usuario", JSON.stringify(usuario));
-    sessionStorage.removeItem("usuario.primeraVez")
+    sessionStorage.removeItem("usuario.primeraVez");
   }
 }
 
-//Saluda, declara y modifica mas variables 
+//Saluda, declara, pone el nombre de usuario y modifica mas variables 
 if (localStorage.usuario !== undefined) {
   usuario = JSON.parse(localStorage.getItem("usuario"));
   cuenta = true;
+  //Pone el nombre de usuario localStorage
+  document.getElementById("usuario").innerHTML = usuario.nombre;
+  //Saludo localStorage
   if (cuenta && usuario.primeraVez) {
-    alert("Bienvenido/a " + usuario.nombre);
-    usuario.primeraVez = false
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Bienvenido ' + usuario.nombre,
+      showConfirmButton: false,
+      timer: 3000
+    })
+    usuario.primeraVez = false;
     sessionStorage.setItem("usuario", JSON.stringify(usuario));
-    sessionStorage.removeItem("usuario.primeraVez")
+    sessionStorage.removeItem("usuario.primeraVez");
   }
 }
 
@@ -83,4 +101,16 @@ function desconectarse() {
     }
     cuenta = false;
     logueado();
+}
+//Estética de desconexión
+let clicksAUsuario = 0
+function clickUsuario(){
+  if (clicksAUsuario===0) {
+    document.getElementById("nav").innerHTML += `<button class="py-2 d-none d-md-inline-block" id="desconectarse" onclick="desconectarse()" onmouseover="sadMoment(true)" onmouseleave="sadMoment(false)" style="color: white; background-color: rgba(0, 0, 0, 0); background-image: url('img/desconectar.jpg'); border-color: rgb(255, 0, 0); background-size: cover;" >Desconectarse</button>`
+    clicksAUsuario = 1
+  }else{
+    document.getElementById("nav").removeChild(document.getElementById("desconectarse"))
+    clicksAUsuario = 0
+  }
+  
 }
--- js/products.js
+++ js/products.js
@@ -1,12 +1,17 @@
 const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_COUNT = "Cant.";
-var currentCategoriesArray = [];
+const ORDER_BY_PROD_COST = "Cost.";
+const ORDER_BY_PROD_SOLD = "SoldCount."
+var currentProductsArray = [];
 var currentSortCriteria = undefined;
-var minCount = undefined;
-var maxCount = undefined;
+var minPrice = undefined;
+var maxPrice = undefined;
+var productName = undefined;
+//Contadores para la estética de las listas vacias
+let contadorEstetico = -2
+//Fin de contadores estéticos
 
-function sortCategories(criteria, array){
+//Función que ordena listas de productos
+function sortProducts(criteria, array){
     let result = [];
     if (criteria === ORDER_ASC_BY_NAME)
     {
@@ -15,19 +20,23 @@ function sortCategories(criteria, array){
             if ( a.name > b.name ){ return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_DESC_BY_NAME){
+    }else if (criteria === ORDER_BY_PROD_COST){
         result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
+            let aPrice = parseInt(a.cost);
+            let bPrice = parseInt(b.cost);
+
+            if ( aPrice > bPrice ){ return -1; }
+            if ( aPrice < bPrice ){ return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
+        
+    }else if (criteria === ORDER_BY_PROD_SOLD) {
         result = array.sort(function(a, b) {
-            let aCount = parseInt(a.productCount);
-            let bCount = parseInt(b.productCount);
+            let aSoldCount = parseInt(a.soldCount);
+            let bSoldCount = parseInt(b.soldCount);
 
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
+            if ( aSoldCount > bSoldCount){ return -1; }
+            if ( aSoldCount < bSoldCount){ return 1; }
             return 0;
         });
     }
@@ -35,103 +44,186 @@ function sortCategories(criteria, array){
     return result;
 }
 
-//Muestra la lista de productos que está en el json
-function showCategoriesList(){
+//Función que filtra la lista de productos que está en el json y luego la muestra
+function showProductsList(){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentCategoriesArray.length; i++){
-        let category = currentCategoriesArray[i];
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let product = currentProductsArray[i];
 
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+        if (((minPrice == undefined) || (minPrice != undefined && parseInt(product.cost) >= minPrice)) &&
+            ((maxPrice == undefined) || (maxPrice != undefined && parseInt(product.cost) <= maxPrice))&&
+            ((productName == undefined) || (productName != undefined && product.name.toLowerCase().includes(productName)))){
 
             htmlContentToAppend += `
             <a href="category-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ category.name +`</h4>
-                            <small>` + `<span style="font-size: 30px;">`+ "<strong>" + category.currency+ " " + category.cost + "</strong>"+ "<br>" +`</span>` + category.soldCount + ` artículos vendidos</small>
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small>` + `<span style="font-size: 30px;">`+ "<strong>" + product.currency+ " " + product.cost + "</strong>"+ "<br>" +`</span>` + product.soldCount + ` artículos vendidos</small>
                         </div>
-                        <p class="mb-1">` + category.description + `</p>
+                        <p class="mb-1">` + product.description + `</p>
                     </div>
                 </div>
             </a>
             `
         }
-
+        
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
     }
+
+
+    //Pura estética para cuando se muestra una lista vacia
+    let listaEstetica1 = ['img/meme.gif', 'img/no_result.gif']
+    let listaEstetica2 = ['img/pepe.gif', 'img/pepe1.gif']
+    let listaEstetica3 = ['img/pepe2.gif', 'img/windows95.gif']
+    let listaDeListasEsteticas = [listaEstetica1, listaEstetica2, listaEstetica3]
+    if (contadorEstetico===3) {
+        contadorEstetico=-2
+    }
+    if (htmlContentToAppend === "" && contadorEstetico < 3 && contadorEstetico >= 0) {
+        document.getElementById("cat-list-container").innerHTML+=`<p style='font-size: 2cm; background-color:red;'><strong>No hay resultados</strong><img style='width: 4cm; height: 4cm;' src=` + (listaDeListasEsteticas[contadorEstetico])[0] + `></p><img style='width:20cm; height:4cm;' src=` + (listaDeListasEsteticas[contadorEstetico])[1] + `>`
+        contadorEstetico+=1
+    }
+    if (htmlContentToAppend === "" && contadorEstetico <0 ) {
+        document.getElementById("cat-list-container").innerHTML +=`<p style='font-size: 2cm; background-color:red;'><strong>No hay resultados</strong></p>`
+        contadorEstetico+=1
+    }
+    
+    if (contadorEstetico===3) {
+        Swal.fire({
+            title: 'Desarrolladores ahora',
+            text: '(Federico)',
+            imageUrl: 'img/pepe4.gif',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+          })
+    }
+    //Fin de la estética de la lista vacia
 }
 
-function sortAndShowCategories(sortCriteria, categoriesArray){
+//Función que ordena y muestra listas de productos
+let times_Pressed = 0
+function sortAndShowProducts(sortCriteria, productsArray){
     currentSortCriteria = sortCriteria;
 
-    if(categoriesArray != undefined){
-        currentCategoriesArray = categoriesArray;
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
     }
+    //Ordena la lista de productos
+    currentProductsArray= sortProducts(currentSortCriteria, currentProductsArray);
 
-    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
-
-    //Muestro las categorías ordenadas
-    showCategoriesList();
+    
+    
+        if (times_Pressed === 0) {
+            times_Pressed=1
+        }else{
+            currentProductsArray.reverse()
+            times_Pressed=0
+        }
+    //Muestra la lista de productos filtrada anteriormente ordenada
+    showProductsList();
 }
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+//Establece los eventos para las etiquetas html que los necesiten y obtiene los datos del json
 document.addEventListener("DOMContentLoaded", function(e){
+
+    //Toma los datos del json y los pone en un objeto
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
-            sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+            sortAndShowProducts(ORDER_ASC_BY_NAME, resultObj.data);
         }
     });
 
+    //añade el evento que necesita boton de ordenar alfabeticamente
     document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_ASC_BY_NAME);
+        sortAndShowProducts(ORDER_ASC_BY_NAME);
+        ascDsc(true, 'sortAsc');
+        ascDsc(false, 'sortByCost');
+        ascDsc(false, 'sortByProdSold');
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_DESC_BY_NAME);
+    //añade el evento que necesita boton de ordenar por costo
+    document.getElementById("sortByCost").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_BY_PROD_COST);
+        ascDsc(true, 'sortByCost');
+        ascDsc(false, 'sortAsc');
+        ascDsc(false, 'sortByProdSold');
     });
 
-    document.getElementById("sortByCount").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_BY_PROD_COUNT);
+    //añade el evento que necesita boton de ordenar por relevancia
+    document.getElementById("sortByProdSold").addEventListener("click", function(){
+        sortAndShowProducts(ORDER_BY_PROD_SOLD);
+        ascDsc(true, 'sortByProdSold');
+        ascDsc(false, 'sortAsc');
+        ascDsc(false, 'sortByCost');
     });
 
+    //añade el evento que necesita boton de limpiar
     document.getElementById("clearRangeFilter").addEventListener("click", function(){
-        document.getElementById("rangeFilterCountMin").value = "";
-        document.getElementById("rangeFilterCountMax").value = "";
+        document.getElementById("rangeFilterPriceMin").value = "";
+        document.getElementById("rangeFilterPriceMax").value = "";
+        document.getElementById("search").value = "";
 
-        minCount = undefined;
-        maxCount = undefined;
+        minPrice = undefined;
+        maxPrice = undefined;
+        productName = undefined;
 
-        showCategoriesList();
+        showProductsList();
     });
 
-    document.getElementById("rangeFilterCount").addEventListener("click", function(){
-        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
-        //de productos por categoría.
-        minCount = document.getElementById("rangeFilterCountMin").value;
-        maxCount = document.getElementById("rangeFilterCountMax").value;
+    //añade el evento que necesita boton de filtrar ya sea por precio o por nombre
+    document.getElementById("rangeFilterPrice").addEventListener("click", function(){
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por precio de los productos
+        minPrice = document.getElementById("rangeFilterPriceMin").value;
+        maxPrice = document.getElementById("rangeFilterPriceMax").value;
+        productName = document.getElementById("search").value.toLowerCase();
 
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-            minCount = parseInt(minCount);
+        if ((minPrice != undefined) && (minPrice != "") && (parseInt(minPrice)) >= 0){
+            minPrice = parseInt(minPrice);
         }
         else{
-            minCount = undefined;
+            minPrice = undefined;
         }
 
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-            maxCount = parseInt(maxCount);
+        if ((maxPrice != undefined) && (maxPrice != "") && (parseInt(maxPrice)) >= 0){
+            maxPrice = parseInt(maxPrice);
         }
         else{
-            maxCount = undefined;
+            maxPrice = undefined;
         }
 
-        showCategoriesList();
+        showProductsList();
+    });
+
+    document.getElementById("search").addEventListener("keyup", function(){
+        productName = document.getElementById("search").value.toLowerCase();
+        showProductsList();
     });
 });
+//Fin de las asignaciones de eventos
+
+
+//detalle estético de las etiquetas que ordenan
+function ascDsc(booleano, id){
+    if (booleano) {
+        if (times_Pressed === 1) {
+            document.getElementById(id).style = "color: rgb(0, 0, 0); background-image:url('img/flecha_dsc.jpg'); border-color: rgb(255, 0, 0); background-size: cover; background-position: center;";
+        }else{
+      document.getElementById(id).style = "color: rgb(0, 0, 0); background-image: url('img/flecha_asc.jpg'); border-color: rgb(255, 0, 0); background-size: cover;"
+        }
+    }else{
+        document.getElementById(id).style=""
+    }
+}
+//fin del detalle estético
